cdaa0bdb5bf540948e081644bdb2bd76bb04e3a6
israel <israel.bejarano.a@gmail.com>
2019-07-28 10:53:27 +0200

    refactoracion de codigo

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroe',
@@ -25,18 +27,29 @@ export class HeroeComponent implements OnInit {
     console.log(form);
     console.log(this.heroe);
 
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      type: 'info',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
     if (this.heroe.id) {
-      this.heroesService.actualizarHeroe(this.heroe)
-        .subscribe(resp => {
-          console.log(resp);
-        });
+      peticion = this.heroesService.actualizarHeroe(this.heroe);
     } else {
-      this.heroesService.crearHeroe(this.heroe)
-        .subscribe(resp => {
-          console.log(resp);
-          this.heroe = resp;
-        });
+      peticion = this.heroesService.crearHeroe(this.heroe);
     }
+
+    peticion.subscribe(resp => {
+      Swal.fire({
+        title: this.heroe.nombre,
+        text: 'Se actualizó correctamente',
+        type: 'success'
+      });
+    });
 
   }
 
